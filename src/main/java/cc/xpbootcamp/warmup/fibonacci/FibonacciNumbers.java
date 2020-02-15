531d64746754fd7286139dab14e4963719b45387
YuLing Liu <yuling.liu@thoughtworks.com>
2020-02-15 19:18:29 +0800

    Passed unit test when calcaute 1 position value with FibonacciNumber

---
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciNumbers.java
@@ -0,0 +1,7 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+public class FibonacciNumbers {
+    public long calculate(int i) {
+        return 1;
+    }
+}
---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciNumbersTest.java
@@ -0,0 +1,23 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class FibonacciNumbersTest {
+    FibonacciNumbers fn;
+
+    @Before
+    public void setUp(){
+        fn = new FibonacciNumbers();
+    }
+
+    @Test
+    public void should_return_1_when_calculate_value_given_1_position(){
+        long value = fn.calculate(0);
+        assertEquals(1, value);
+    }
+
+}
