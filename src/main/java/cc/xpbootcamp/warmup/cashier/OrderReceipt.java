3750c2e98513ef6ab8c81433f87db317aae4cdce
YuLing Liu <yuling.liu@thoughtworks.com>
2020-02-19 16:21:10 +0800

    Refactor code

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,5 +1,9 @@
 package cc.xpbootcamp.warmup.cashier;
 
+import cc.xpbootcamp.warmup.cashier.utils.DateUtils;
+
+import java.util.Date;
+
 /**
  * OrderReceipt prints the details of order including customer name, address, description, quantity,
  * price and amount. It also calculates the sales tax @ 10% and prints as part
@@ -8,7 +12,7 @@ package cc.xpbootcamp.warmup.cashier;
  */
 public class OrderReceipt {
     private Order order;
-    private static String HEADER = "======Printing Orders======" + '\n';
+    private static String HEADER = "===== 老王超市，值得信赖 ======" + '\n'+'\n';
     private String baseInfo;
     private String orderItemsInfo;
     private String orderSummarise;
@@ -19,19 +23,19 @@ public class OrderReceipt {
     }
 
     public String printReceipt() {
+    return HEADER +
+//        formatOrderCreateDate() +
+        initOrderBaseInfo() +
+        initOrderItemsInfo() +
+        initOrderSummarise() ;
 
-        initOrderBaseInfo();
-        initOrderItemsInfo();
-        initOrderSummarise();
-
-        return HEADER + baseInfo + orderItemsInfo + orderSummarise;
     }
 
-    private void initOrderBaseInfo() {
-        baseInfo = order.getCustomerName() + order.getCustomerAddress();
+    private String initOrderBaseInfo() {
+         return baseInfo = order.getCustomerName() + order.getCustomerAddress();
     }
 
-    private void initOrderItemsInfo() {
+    private String initOrderItemsInfo() {
         for (Item lineItem : order.getLineItems()) {
             String itemInfo = lineItem.getDescription() + '\t'
                     + lineItem.getPrice() + '\t'
@@ -39,52 +43,13 @@ public class OrderReceipt {
                     + lineItem.amount() + '\n';
             baseInfo += itemInfo;
         }
+        return baseInfo;
     }
 
-    private void initOrderSummarise() {
+    private String initOrderSummarise() {
         orderSummarise = "Sales Tax" + '\t' + order.getTotalTax()
                 + "Total Amount" + '\t' + order.getTotalAmount();
-    }
-
-    public String printReceiptOlder() {
-        StringBuilder output = new StringBuilder();
-
-        // print headers
-        output.append("======Printing Orders======\n");
-
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(order.getCustomerName());
-        output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
-
-        // prints lineItems
-        double totSalesTx = 0d;
-        double tot = 0d;
-        for (Item lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.amount());
-            output.append('\n');
-
-            // calculate sales tax @ rate of 10%
-            double salesTax = lineItem.amount() * .10;
-            totSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.amount() + salesTax;
-        }
-
-        // prints the state tax
-        output.append("Sales Tax").append('\t').append(totSalesTx);
-
-        // print total amount
-        output.append("Total Amount").append('\t').append(tot);
-        return output.toString();
+        return orderSummarise;
     }
 
 }
